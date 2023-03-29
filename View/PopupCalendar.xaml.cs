cbc7abf98e63bf701fd10fdb892e55eb13cf8cb8
ddr97235@hotmail.com <ddr97235@hotmail.com>
2023-03-29 19:21:05 +0300

    Добавьте файлы проекта.

---
+++ View/PopupCalendar.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace CalendarApplication.View
+{
+    public partial class PopupCalendar : UserControl
+    {
+        public DateTime Date
+        {
+            get { return (DateTime)GetValue(DateProperty); }
+            set { SetValue(DateProperty, value); }
+        }
+        public static DependencyProperty DateProperty = DependencyProperty.Register("Date", typeof(DateTime), typeof(PopupCalendar), 
+            new FrameworkPropertyMetadata(new PropertyChangedCallback(OnDatePropertyChanged)/*, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault*/));
+
+        private static void OnDatePropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            PopupCalendar CurrentPopupCalendar = (PopupCalendar)sender;
+        }
+
+        public PopupCalendar()
+        {
+            InitializeComponent();
+            Date = DateTime.Now;
+        }
+
+        private void TEXT_DATE_MouseUP(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                POPUP_PATH.IsOpen = true;
+            }
+        }
+
+        private void calenrdar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            POPUP_PATH.IsOpen = false;
+            Date = ((Calendar)sender).SelectedDate!.Value;
+        }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using CalendarApplication.ViewModel.Simplified;
+using System.Windows;
+using System.Threading;
+using System.Collections;
+
+namespace CalendarApplication.ViewModel
+{
+    internal class MainWindowViewModel : BaseInpc
+    {
+
+        private DateTime date =DateTime.Now;
+        public DateTime Date
+        {
+            get => date;
+            set => Set(ref date, value);
+        }
+
+
+
+    }
+}
